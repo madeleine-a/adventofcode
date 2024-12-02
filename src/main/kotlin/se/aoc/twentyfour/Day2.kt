dd964386dc1f197008afebd2221f469445751716
Madeleine Andersson <andersson.madeleine@gmail.com>
2024-12-02 12:38:35 +0100

    2024 - Day 1 and 2

--- src/main/kotlin/se/aoc/twentyfour/Day2.kt
+++ src/main/kotlin/se/aoc/twentyfour/Day2.kt
@@ -19,59 +19,6 @@ class Day2 {
         println(part2(list)) // 54518
     }
 
-    private fun isSafe(list: List<Int>): Boolean {
-        return isIncreasing(list).map { it.second }.reduce{ a,b -> b && a } ||
-                isDecreasing(list).map{it.second }.reduce{ a,b -> b && a }
-    }
-
-    private fun isSafeWithProblemDamper(list: List<Int>): Boolean {
-        val incResult = isIncreasing(list)
-        val decResult = isDecreasing(list)
-        val inc = incResult.map { it.second }.reduce{ a,b -> b && a }
-        val dec = decResult.map{it.second }.reduce{ a,b -> b && a }
-        if(inc || dec){
-            return true
-        } else {
-            for (i in list.indices){
-                val newList = list.mapIndexedNotNull{ index, value -> if(index == i) null else value }
-
-                if(isIncreasing(newList).map { it.second }.reduce{ a,b -> b && a } ||
-                        isDecreasing(newList).map{it.second }.reduce{ a,b -> b && a }){
-                    return true
-                }
-            }
-            return false
-        }
-    }
-
-
-    private fun isIncreasing(list: List<Int>): List<Pair<String, Boolean>> {
-        val result = list.subList(0, list.size - 1).mapIndexed { index, i ->
-            val value = (i - list[index + 1]).absoluteValue
-            if (i <= list[index + 1]) {
-                "NOT" to false
-            } else if (value < 1 || value > 3) {
-                "DIFF" to false
-            } else {
-                "OK" to true
-            }
-        }
-        return result
-    }
-
-    private fun isDecreasing(list: List<Int>): List<Pair<String, Boolean>> {
-        val result = list.subList(0, list.size - 1).mapIndexed { index, i ->
-            val value = (i - list[index + 1]).absoluteValue
-            if (i >= list[index + 1]) {
-                "NOT" to false
-            } else if (value < 1 || value > 3) {
-                "DIFF" to false
-            } else {
-                "OK" to true
-            }
-        }
-       return result
-    }
 
     fun createList(str: String): List<List<Int>> {
         val list = str.split("\n").map {
@@ -89,5 +36,39 @@ class Day2 {
     fun part2(list: List<List<Int>>): Number {
         val result = list.map { it to isSafeWithProblemDamper(it) }.filter { it.second }
         return result.size
+    }
+
+    private fun isSafe(list: List<Int>): Boolean {
+        return checkList(list, true).map { it.second }.reduce { a, b -> b && a } ||
+                checkList(list, false).map { it.second }.reduce { a, b -> b && a }
+    }
+
+    private fun isSafeWithProblemDamper(list: List<Int>): Boolean {
+        if (isSafe(list)) {
+            return true
+        } else {
+            for (i in list.indices) {
+                val newList = list.mapIndexedNotNull { index, value -> if (index == i) null else value }
+                if (isSafe(newList)) {
+                    return true
+                }
+            }
+            return false
+        }
+    }
+
+
+    private fun checkList(list: List<Int>, doUp: Boolean): List<Pair<String, Boolean>> {
+        val result = list.subList(0, list.size - 1).mapIndexed { index, i ->
+            val value = (i - list[index + 1]).absoluteValue
+            if ((doUp && i <= list[index + 1]) || (!doUp && i >= list[index + 1])) {
+                "NOT" to false
+            } else if (value < 1 || value > 3) {
+                "DIFF" to false
+            } else {
+                "OK" to true
+            }
+        }
+        return result
     }
 }
